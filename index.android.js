da42f6bea0550986b46ba6b69d0165fcd8e062fd
santosh sharma <santosh.sharma@botreetechnologies.com>
2017-05-19 18:19:58 +0530

    react native wifi functionality

---
+++ index.android.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Modal,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+  ScrollView,
+  View
+} from 'react-native';
+import wifi from 'react-native-android-wifi';
+import { Container, Content, Picker, List, ListItem, Button, Text, InputGroup, Input, Form, Item } from 'native-base';
+const Itemp = Picker.Item
+
+
+export default class WifiApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected1: '0',
+      wifiList: [],
+      password: '',
+      ssid: ''
+    }
+  }
+
+  onValueChange(value: string) {
+    console.log('value-----------' + value);
+    this.setState({
+      selected1: value
+    });
+    let selectedSsid = this.state.wifiList[value];
+
+    console.log('ssid----------' + selectedSsid.SSID)
+
+    this.setState({ ssid: selectedSsid.SSID })
+  }
+
+  getWifiNetworks() {
+    wifi.loadWifiList((wifiStringList) => {
+      var wifiArray = JSON.parse(wifiStringList);
+      this.setState({
+        wifiList: wifiArray,
+      });
+      let selectedSsidDefault = this.state.wifiList[this.state.selected1];
+      this.setState({ ssid: selectedSsidDefault.SSID });
+
+      console.log(this.state.wifiList);
+
+      console.log('------------------' + this.state.ssid);
+    },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  connectOnPress() {
+    wifi.findAndConnect(this.state.ssid, this.state.password, (found) => {
+      if (found) {
+        console.log('connected successfully...');
+      } else {
+        console.log('unable to connect....');
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getWifiNetworks();
+  }
+
+  render() {
+    return (
+
+      <Container>
+        <Content>
+          <Text>Select Wifi</Text>
+          <Picker
+            supportedOrientations={['portrait', 'landscape']}
+            iosHeader="Select one"
+            mode="dropdown"
+            selectedValue={this.state.selected1}
+            onValueChange={this.onValueChange.bind(this) }>
+            {this.state.wifiList.map((l, i) => { return <Itemp value={i} label={l.SSID} key={i}  /> }) }
+          </Picker>
+          <Form>
+            <Item regular>
+              <Input placeholder='Password' secureTextEntry={true} onChangeText={(password) => this.setState({ password }) }/>
+            </Item>
+          </Form>
+          <Button block style={{ margin: 20, top: 20 }} onPress={this.connectOnPress.bind(this) }><Text> Connect </Text></Button>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+AppRegistry.registerComponent('WifiApp', () => WifiApp);
